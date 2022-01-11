a8c843ac7167581a2fe413cac936732d99e6552b
kurniacf <kcf170202@gmail.com>
2022-01-11 08:33:36 +0700

    overriding

--- #5_overriding/over.php
+++ #5_overriding/over.php
@@ -53,7 +53,7 @@ class ReviewerMovie extends Movie
 
     public function message()
     {
-        return "Get Free Photo from $this->actor";
+        return "Get Free Movie";
     }
 }
 
@@ -61,10 +61,11 @@ $streaming1 = new Movie("Venom", "Tom Hardy");
 $streaming2 = new Movie("Loki", "Tom Hiddleston");
 $streaming3 = new ReviewerMovie("Squid Game", "HoYeon Jung", 9.35);
 
+echo $streaming1->subscription . "<br>";
+echo $streaming3->subscription . "<br>";
 
-echo $streaming3->name . "<br>";
-echo $streaming3->rating . "<br>";
-echo $streaming3->getActor() . "<br>";
+echo $streaming1->message() . "<br>";
+echo $streaming3->message() . "<br>";
 
 ?>
 
@@ -75,7 +76,7 @@ echo $streaming3->getActor() . "<br>";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Class Tutorial</title>
+    <title>Overriding Tutorial</title>
 </head>
 
 <body>
